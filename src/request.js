9f2011a16b8fba3bf17ea4b7d918b3f7a945d507
Matt Fysh <mattfysh@gmail.com>
2021-11-27 11:15:47 +1000

    attach reused_socket to request span

--- src/request.js
+++ src/request.js
@@ -1,14 +1,22 @@
 import * as Sentry from '@sentry/node'
 import http from 'http'
 
-const agent = new http.Agent({ keepAlive: true })
+class MyAgent extends http.Agent {
+  addRequest(...args) {
+    super.addRequest(...args)
+    const [req] = args
+    const scope = Sentry.getCurrentHub().getScope();
+    const { spans } = scope.getSpan().spanRecorder
+    const requestSpan = spans[spans.length - 1]
+    requestSpan.setTag('http.reused_socket', req.reusedSocket)
+    return req
+  }
+}
+
+const agent = new MyAgent({ keepAlive: true })
 
 export const getExample = async () => {
   const req = http.request('http://example.com', { agent })
-
-  Sentry.configureScope(scope => {
-    scope.setTag('http.reused_socket', req.reusedSocket)
-  })
 
   return new Promise(resolve => {
     req.on('response', res => {
